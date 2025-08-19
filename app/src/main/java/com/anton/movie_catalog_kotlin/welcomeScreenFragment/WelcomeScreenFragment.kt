44fa7992ec78ed98ec9dd004214a50733bd76c29
Anton Ber <holzed15@gmail.com>
2025-08-19 21:20:50 +0300

    навигация с welcome screen на другие экраны

--- app/src/main/java/com/anton/movie_catalog_kotlin/welcomeScreenFragment/WelcomeScreenFragment.kt
+++ app/src/main/java/com/anton/movie_catalog_kotlin/welcomeScreenFragment/WelcomeScreenFragment.kt
@@ -3,6 +3,7 @@ package com.anton.movie_catalog_kotlin.welcomeScreenFragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.anton.movie_catalog_kotlin.R
 import com.anton.movie_catalog_kotlin.databinding.WelcomeScreenFragmentBinding
 
@@ -14,5 +15,14 @@ class WelcomeScreenFragment() : Fragment(R.layout.welcome_screen_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = WelcomeScreenFragmentBinding.bind(view)
+
+        with(binding) {
+            buttonEnterAccount.setOnClickListener {
+                findNavController().navigate(R.id.action_welcome_screen_fragment_to_signInFragment)
+            }
+            buttonSignUpAccount.setOnClickListener {
+                findNavController().navigate(R.id.action_welcome_screen_fragment_to_signUpFragment)
+            }
+        }
     }
 }
