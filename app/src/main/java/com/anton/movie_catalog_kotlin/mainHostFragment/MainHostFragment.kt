f1659735b9c578dd23031189130455414a00a2fc
Anton Ber <holzed15@gmail.com>
2025-08-23 21:16:08 +0300

    навигационный бар

--- app/src/main/java/com/anton/movie_catalog_kotlin/mainHostFragment/MainHostFragment.kt
+++ app/src/main/java/com/anton/movie_catalog_kotlin/mainHostFragment/MainHostFragment.kt
@@ -3,6 +3,8 @@ package com.anton.movie_catalog_kotlin.mainHostFragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.anton.movie_catalog_kotlin.R
 import com.anton.movie_catalog_kotlin.databinding.MainHostFragmentBinding
 
@@ -14,5 +16,11 @@ class MainHostFragment : Fragment(R.layout.main_host_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = MainHostFragmentBinding.bind(view)
+
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = binding.bottomNavigationView
+        bottomNavigationView.setupWithNavController(navController)
     }
+
 }
