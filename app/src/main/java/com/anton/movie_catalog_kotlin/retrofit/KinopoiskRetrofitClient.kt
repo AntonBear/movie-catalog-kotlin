b593035b58720b47c273556cacacd226f4b36171
Anton Ber <holzed15@gmail.com>
2025-08-24 18:42:40 +0300

    тест работает

--- app/src/main/java/com/anton/movie_catalog_kotlin/retrofit/KinopoiskRetrofitClient.kt
+++ app/src/main/java/com/anton/movie_catalog_kotlin/retrofit/KinopoiskRetrofitClient.kt
@@ -2,14 +2,25 @@ package com.anton.movie_catalog_kotlin.retrofit
 
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 object KinopoiskRetrofitClient {
     private val json = Json { ignoreUnknownKeys = true }
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("X-API-KEY", "5673684a-da0e-43e0-bfc9-4829489bbe4f")
+                .build()
+            chain.proceed(request)
+        }
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://kinopoiskapiunofficial.tech/")
+        .client(okHttpClient)
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .build()
 
--- app/src/main/java/com/anton/movie_catalog_kotlin/retrofit/KreosoftRetrofitClient.kt
+++ app/src/main/java/com/anton/movie_catalog_kotlin/retrofit/KreosoftRetrofitClient.kt
@@ -4,12 +4,23 @@ import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 
 object KreosoftRetrofitClient {
     private val json = Json { ignoreUnknownKeys = true }
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("X-API-KEY", "5673684a-da0e-43e0-bfc9-4829489bbe4f")
+                .build()
+            chain.proceed(request)
+        }
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://react-midterm.kreosoft.space/")
+        .client(okHttpClient)
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .build()
 
