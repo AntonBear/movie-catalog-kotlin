7031a1049aa79cac0190aeafb2fbc38cfb125a2c
Anton Ber <holzed15@gmail.com>
2025-09-10 21:12:37 +0300

    добавлены чипсы

--- app/src/main/java/com/anton/movie_catalog_kotlin/feedScreenFragment/FeedScreenFragment.kt
+++ app/src/main/java/com/anton/movie_catalog_kotlin/feedScreenFragment/FeedScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.anton.movie_catalog_kotlin.feedScreenFragment
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.SpannableString
@@ -16,16 +17,20 @@ import androidx.lifecycle.repeatOnLifecycle
 import coil.load
 import com.anton.movie_catalog_kotlin.R
 import com.anton.movie_catalog_kotlin.databinding.FeedScreenFragmentBinding
+import com.anton.movie_catalog_kotlin.models_old.Genre
+import com.anton.movie_catalog_kotlin.models_old.GenreModel
 import com.anton.movie_catalog_kotlin.movieDetailsScreen.MovieDetailsActivity
 import com.bumptech.glide.Glide
 import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class FeedScreenFragment : Fragment(R.layout.feed_screen_fragment) {
-    private val viewModel: FeedScreenFragmentViewModel by viewModels()
 
+    private val viewModel: FeedScreenFragmentViewModel by viewModels()
     private var _binding: FeedScreenFragmentBinding? = null
     private val binding get() = _binding!!
 
@@ -39,37 +44,71 @@ class FeedScreenFragment : Fragment(R.layout.feed_screen_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("DEBUG", "FeedScreenFragment - onViewCreated")
 
+        // Подписка на Flow
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-                    viewModel.movieData.collect {
-                        binding.movieTitle.text = it?.name ?: "Загрузка"
-                    }
-                }
-                launch {
-                    viewModel.movieData.collect { movieDetails ->
+                viewModel.movieData.collectLatest { movie ->
+                    movie?.let {
+                        binding.movieTitle.text = it.name
+                        binding.movieCountryYear.text = "${it.country} • ${it.year}"
+
                         Glide.with(binding.root.context)
-                            .load(movieDetails?.poster)
+                            .load(it.poster)
                             .error(R.drawable.error_image)
                             .into(binding.moviePoster)
-                    }
-                }
-                launch {
-                    viewModel.movieData.collect { movieDetails ->
-                        binding.movieCountryYear.text =  "${movieDetails?.country ?: ""} • ${movieDetails?.year ?: ""}"
-                    }
-                }
-                launch {
 
+                        // Жанры
+                        binding.genreChipGroup.removeAllViews()
+                        setChipsSingleLine(binding.genreChipGroup,movie.genres)
+//                        it.genres.forEach { genre ->
+//                            val chip = Chip(binding.root.context).apply {
+//                                text = genre.name
+//                                isCheckable = false
+//                            }
+//                            binding.genreChipGroup.addView(chip)
+//                        }
+                    }
                 }
             }
         }
+    }
+
+    override fun onResume() {
+        viewModel.loadData() // триггерим обновление при возврате
+        super.onResume()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
+
+    fun setChipsSingleLine(chipGroup: ChipGroup, genres: List<GenreModel>) {
+        chipGroup.removeAllViews()
+
+        chipGroup.post { // ждем, пока layout измерит ширину
+            val maxWidth = chipGroup.width
+            var usedWidth = 0
+
+            for (genre in genres) {
+                val chip = Chip(chipGroup.context).apply { text = genre.name }
+
+                chip.measure(
+                    View.MeasureSpec.UNSPECIFIED,
+                    View.MeasureSpec.UNSPECIFIED
+                )
+                val chipWidth = chip.measuredWidth
+
+                if (usedWidth + chipWidth > maxWidth) break
+
+                usedWidth += chipWidth
+                chipGroup.addView(chip)
+            }
+        }
+    }
+
+
 }
