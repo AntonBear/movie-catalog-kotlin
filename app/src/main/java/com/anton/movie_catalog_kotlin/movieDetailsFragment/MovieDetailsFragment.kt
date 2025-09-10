9018c9ae12a610619179628abd0c40aefb08cbdc
Anton Ber <holzed15@gmail.com>
2025-09-10 22:33:51 +0300

    поправил навигацию

--- app/src/main/java/com/anton/movie_catalog_kotlin/movieDetailsFragment/MovieDetailsFragment.kt
+++ app/src/main/java/com/anton/movie_catalog_kotlin/movieDetailsFragment/MovieDetailsFragment.kt
@@ -10,24 +10,24 @@ import androidx.fragment.app.Fragment
 import com.anton.movie_catalog_kotlin.R
 import com.anton.movie_catalog_kotlin.databinding.MoviesScreenFragmentBinding
 import com.anton.movie_catalog_kotlin.movieDetailsScreen.MovieDetailsScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MovieDetailsFragment : Fragment() {
 
-    private var _binding: MoviesScreenFragmentBinding? = null
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        return ComposeView(requireContext()).apply {
-//            setContent {}
-//        }
-//    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {}
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = MoviesScreenFragmentBinding.bind(view)
         Log.d("DEBUG", "App started - check if changes are applied")
 
     }
