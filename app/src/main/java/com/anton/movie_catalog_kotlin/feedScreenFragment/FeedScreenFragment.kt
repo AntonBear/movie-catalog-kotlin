285f28d0f77bdc02a9aa7acb18bfdad421aabeff
Anton Ber <holzed15@gmail.com>
2025-09-10 21:19:36 +0300

    исправил чипсы

--- app/src/main/java/com/anton/movie_catalog_kotlin/feedScreenFragment/FeedScreenFragment.kt
+++ app/src/main/java/com/anton/movie_catalog_kotlin/feedScreenFragment/FeedScreenFragment.kt
@@ -1,28 +1,18 @@
 package com.anton.movie_catalog_kotlin.feedScreenFragment
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.text.SpannableString
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import coil.load
 import com.anton.movie_catalog_kotlin.R
 import com.anton.movie_catalog_kotlin.databinding.FeedScreenFragmentBinding
-import com.anton.movie_catalog_kotlin.models_old.Genre
-import com.anton.movie_catalog_kotlin.models_old.GenreModel
-import com.anton.movie_catalog_kotlin.movieDetailsScreen.MovieDetailsActivity
 import com.bumptech.glide.Glide
 import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipGroup
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -58,16 +48,15 @@ class FeedScreenFragment : Fragment(R.layout.feed_screen_fragment) {
                             .error(R.drawable.error_image)
                             .into(binding.moviePoster)
 
-                        // Жанры
                         binding.genreChipGroup.removeAllViews()
-                        setChipsSingleLine(binding.genreChipGroup,movie.genres)
-//                        it.genres.forEach { genre ->
-//                            val chip = Chip(binding.root.context).apply {
-//                                text = genre.name
-//                                isCheckable = false
-//                            }
-//                            binding.genreChipGroup.addView(chip)
-//                        }
+
+                        it.genres.take(3).forEach { genre ->
+                            val chip = Chip(binding.root.context).apply {
+                                text = genre.name
+                                isCheckable = false
+                            }
+                            binding.genreChipGroup.addView(chip)
+                        }
                     }
                 }
             }
@@ -75,7 +64,7 @@ class FeedScreenFragment : Fragment(R.layout.feed_screen_fragment) {
     }
 
     override fun onResume() {
-        viewModel.loadData() // триггерим обновление при возврате
+        viewModel.loadData()
         super.onResume()
     }
 
@@ -83,32 +72,4 @@ class FeedScreenFragment : Fragment(R.layout.feed_screen_fragment) {
         super.onDestroyView()
         _binding = null
     }
-
-
-
-    fun setChipsSingleLine(chipGroup: ChipGroup, genres: List<GenreModel>) {
-        chipGroup.removeAllViews()
-
-        chipGroup.post { // ждем, пока layout измерит ширину
-            val maxWidth = chipGroup.width
-            var usedWidth = 0
-
-            for (genre in genres) {
-                val chip = Chip(chipGroup.context).apply { text = genre.name }
-
-                chip.measure(
-                    View.MeasureSpec.UNSPECIFIED,
-                    View.MeasureSpec.UNSPECIFIED
-                )
-                val chipWidth = chip.measuredWidth
-
-                if (usedWidth + chipWidth > maxWidth) break
-
-                usedWidth += chipWidth
-                chipGroup.addView(chip)
-            }
-        }
-    }
-
-
 }
